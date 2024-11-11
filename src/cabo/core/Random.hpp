965019ab34f6d58101688fc36bc1fe3ae9a20c3b
ddimos <ddimasul@gmail.com>
2024-11-11 23:46:51 +0100

    Add Random class

---
+++ src/cabo/core/Random.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include <random>
+
+namespace cn::core
+{
+
+class Random
+{
+public:
+    Random() = default;
+    ~Random() = default;
+
+    void init(unsigned _seed);
+    int rand(int _min, int _max);    // [_min, _max) _max is not included
+
+private:
+    std::mt19937 m_generator;
+};
+
+} // namespace cn::core
