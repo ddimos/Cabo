10db9978e0f83233e339d0f6ac2e2428dc218657
ddimos <ddimasul@gmail.com>
2025-08-17 13:57:56 +0200

    Start implementing flip animation

--- src/cabo/client/game/Card.cpp
+++ src/cabo/client/game/Card.cpp
@@ -8,14 +8,65 @@ namespace cn::client::game
 
 Card::Card(const core::Context& _context, shared::board::Card& _boardCard) 
     : shared::game::Card(_boardCard)
+    , m_interpolatedFlip(_context.get<sf::Clock>(), sf::seconds(1.f), core::Easing::linear)
 {
     m_sprite.setTexture(_context.get<TextureHolder>().get(TextureIds::Cards));
     m_sprite.setTextureRect(game::spriteSheet::getCardBackTextureRect());
     m_sprite.setOrigin(m_sprite.getLocalBounds().getSize() / 2.f);
 }
 
+void Card::startFlipping()
+{
+    m_startFlipping = true;
+    m_isNextFaceUp = !getBoardCard().getFlippableComponent().isFaceUp();
+    m_interpolatedFlip.start(1.f);
+}
+
 void Card::onUpdate(sf::Time _dt)
 {
+    // if (m_startFlipping)
+    // {
+    //     if (m_interpolatedFlip.get() < 0.5f)
+    //     {
+    //         if (m_isNextFaceUp)
+    //         {
+    //             m_sprite.setTextureRect(game::spriteSheet::getCardBackTextureRect());
+    //         }
+    //         else
+    //         {
+    //             if (getBoardCard().getValue().isValid())
+    //             {
+    //                 auto card = shared::game::getCardFromValue(getBoardCard().getValue());
+    //                 m_sprite.setTextureRect(game::spriteSheet::getCardTextureRect(card.first, card.second));
+    //             }
+    //             else
+    //             {
+    //                 m_sprite.setTextureRect(game::spriteSheet::getBlurredCardTextureRect());
+    //             }
+    //         }
+
+    //     }
+    //     else
+    //     {
+    //         if (m_isNextFaceUp)
+    //         {
+    //             if (getBoardCard().getValue().isValid())
+    //             {
+    //                 auto card = shared::game::getCardFromValue(getBoardCard().getValue());
+    //                 m_sprite.setTextureRect(game::spriteSheet::getCardTextureRect(card.first, card.second));
+    //             }
+    //             else
+    //             {
+    //                 m_sprite.setTextureRect(game::spriteSheet::getBlurredCardTextureRect());
+    //             }
+    //         }
+    //         else
+    //         {
+    //             m_sprite.setTextureRect(game::spriteSheet::getCardBackTextureRect());
+    //         }
+    //     }
+    //     m_sprite.setScale(m_interpolatedFlip.get() ,m_sprite.getScale().y);
+    // }
     if (getBoardCard().getFlippableComponent().isFaceUp())
     {
         if (getBoardCard().getValue().isValid())
--- src/cabo/client/game/Card.hpp
+++ src/cabo/client/game/Card.hpp
@@ -2,6 +2,7 @@
 
 #include "core/object/Object.hpp"
 #include "core/Context.hpp"
+#include "core/Interpolator.hpp"
 
 #include "shared/game/Card.hpp"
 
@@ -14,12 +15,17 @@ class Card final : public shared::game::Card
 {
 public:
     Card(const core::Context& _context, shared::board::Card& _boardCard);
-    
+
+    void startFlipping();
+
+private:
     void onUpdate(sf::Time _dt) override;
     void onDraw(sf::RenderWindow& _window) override;
 
-private:
     sf::Sprite m_sprite;
+    core::Interpolator<float> m_interpolatedFlip;
+    bool m_startFlipping = false;
+    bool m_isNextFaceUp = false;
 };
 
 } // namespace cn::client::game
